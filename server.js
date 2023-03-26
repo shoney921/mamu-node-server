7cfdfcb801262dba83d24fa1150a335d20194678
shoney <ddmczp@naver.com>
2023-03-27 00:27:14 +0900

    [feat] 이미지 업로드 구현 및 400에러 추가
    - 이미지 업로드를 위한 로직 추가
    - 에러 발생 시 400 응답

--- server.js
+++ server.js
@@ -4,9 +4,32 @@ const app = express();
 const port = 8080;
 const models = require("./models");
 const art = require("./models/art");
+const multer = require("multer");
+const db = require("./models");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+      const now = new Date();
+      cb(
+        null,
+        now.getFullYear().toString() +
+          now.getMonth().toString() +
+          now.getDate().toString() +
+          now.getHours().toString() +
+          now.getMinutes().toString() +
+          now.getSeconds().toString() +
+          file.originalname
+      );
+    },
+  }),
+});
 
 app.use(express.json());
 app.use(cors());
+app.use("/uploads", express.static("uploads"));
 
 app.get("/arts", (req, res) => {
   models.Art.findAll({
@@ -22,20 +45,21 @@ app.get("/arts", (req, res) => {
     })
     .catch((error) => {
       console.error(error);
-      res.send("findAll 에서 에러 발생");
+      res.status(400).send("findAll 에서 에러 발생");
     });
 });
 
 app.post("/arts", (req, res) => {
   const body = req.body;
-  const { artName, artistName, description } = body;
-  if (!artName || !artistName || !description) {
-    res.send("모든 필드를 입력해주세요");
+  const { artName, artistName, description, imageUrl } = body;
+  if (!artName || !artistName || !description || !imageUrl) {
+    res.status(400).send("모든 필드를 입력해주세요");
   }
   models.Art.create({
     artName,
     artistName,
     description,
+    imageUrl,
   })
     .then((result) => {
       console.log("create result : ", result);
@@ -43,7 +67,7 @@ app.post("/arts", (req, res) => {
     })
     .catch((error) => {
       console.error(error);
-      res.send("업로드에서 문제가 발생했습니다.");
+      res.status(400).send("업로드에서 문제가 발생했습니다.");
     });
 });
 
@@ -61,8 +85,13 @@ app.get("/arts/:id", (req, res) => {
     })
     .catch((error) => {
       console.error(error);
-      res.send("그림 상세조회 에러");
+      res.status(400).send("그림 상세조회 에러");
     });
+});
+
+app.post("/image", upload.single("image"), (req, res) => {
+  const file = req.file;
+  res.send({ imageUrl: file.path });
 });
 
 app.listen(port, () => {
